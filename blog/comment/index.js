a0231ffdf065a96ee50d023e92ecc2795d4cddca
Godfred Nsabo <godfred.nsabo@amalitech.com>
2022-03-24 11:32:03 +0000

    feat: implementing a comments Service

---
+++ blog/comment/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {}; // store all posts created
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({
+    id: commentId,
+    content,
+  });
+  commentsByPostId[req.params.id] = comments;
+  res.status(201).send(comments);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
