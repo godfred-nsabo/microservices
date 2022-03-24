a1046e13f6f3a52032a7e8daa1cc559e4d90caf4
Godfred Nsabo <godfred.nsabo@amalitech.com>
2022-03-24 13:12:29 +0000

    feat: added list of post

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
+
 export default () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Recent Posts</h1>
+      <PostList/>
     </div>
   );
 };
