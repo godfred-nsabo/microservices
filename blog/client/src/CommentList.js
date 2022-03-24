1c86be3b78b194f20e91cf7e0dea88e4f586add6
Godfred Nsabo <godfred.nsabo@amalitech.com>
2022-03-24 15:03:09 +0000

    feat: Displaying Comments on post list

---
+++ blog/client/src/CommentList.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default ({ postId }) => {
+  const [comments, setComments] = useState([]);
+  const fetchDta = async () => {
+    const res = await axios.get(
+      `http://localhost:3001/posts/${postId}/comments`
+    );
+    setComments(res.data);
+  };
+  useEffect(() => {
+    fetchDta();
+  }, []);
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+  return <ul>{renderedComments}</ul>;
+};
